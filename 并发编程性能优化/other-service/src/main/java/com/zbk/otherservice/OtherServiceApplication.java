7f405ef6217cd852340cc3c0735e1afbb4b22926
zhangbokang <540064991@qq.com>
2019-04-21 00:49:22 +0800

    添加ResultBean和注释

--- 并发编程性能优化/other-service/src/main/java/com/zbk/otherservice/OtherServiceApplication.java
+++ 并发编程性能优化/other-service/src/main/java/com/zbk/otherservice/OtherServiceApplication.java
@@ -3,6 +3,12 @@ package com.zbk.otherservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 提供返回结果和暂停几秒模拟慢请求接口
+ *
+ * @author 张卜亢
+ * @date 2019.04.21 00:47:59
+ */
 @SpringBootApplication
 public class OtherServiceApplication {
 
--- 并发编程性能优化/other-service/src/main/java/com/zbk/otherservice/OtherServiceController.java
+++ 并发编程性能优化/other-service/src/main/java/com/zbk/otherservice/OtherServiceController.java
@@ -3,18 +3,25 @@ package com.zbk.otherservice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class OtherServiceController {
     @RequestMapping("/s1")
-    public String service1() throws InterruptedException {
+    public List<ResultBean> service1(String userId) throws InterruptedException {
+        List<ResultBean> resultBeans = new ArrayList<>();
+        resultBeans.add(new ResultBean(userId, userId));
         Thread.sleep(1000L);
-        return "service1";
+        return resultBeans;
     }
 
     @RequestMapping("/s2")
-    public String service2() throws InterruptedException {
+    public List<ResultBean> service2(String moneyId) throws InterruptedException {
+        List<ResultBean> resultBeans = new ArrayList<>();
+        resultBeans.add(new ResultBean(moneyId, moneyId));
         Thread.sleep(2000L);
-        return "service2";
+        return resultBeans;
     }
 }
